b574ea3d269eaeec232568d92c0ffa157b4350bf
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-12-15 12:33:27 +0300

    small changes1

--- migrations/001_init_schema.sql
+++ migrations/001_init_schema.sql
@@ -20,3 +20,15 @@ CREATE TABLE checkingaccount (
     user_id INTEGER REFERENCES users(id),
     balance NUMERIC(10, 2)
 );
+
+CREATE TABLE Transactions (
+    id SERIAL PRIMARY KEY,
+    from_account_id INTEGER,
+    to_account_id INTEGER,
+    amount NUMERIC(10,2),
+    timestamp TIMESTAMP,
+    status TEXT,
+    fee NUMERIC(10,2) NOT NULL DEFAULT 0.00
+)
+
+SELECT id INTO v_system_user_id FROM users WHERE first_name = 'System' AND last_name = 'Bank' LIMIT 1;
--- migrations/003_add_fees.sql
+++ migrations/003_add_fees.sql
@@ -1,9 +1,1 @@
-CREATE TABLE Transactions (
-    id SERIAL PRIMARY KEY,
-    from_account_id INTEGER,
-    to_account_id INTEGER,
-    amount NUMERIC(10, 2),
-    timestamp TIMESTAMP,
-    status TEXT,
-    fee NUMERIC(10, 2) NOT NULL DEFAULT 0.00 -- prevent Oh No migration
-);
+-- creating transactions table moved to 001
